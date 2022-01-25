ddf782e90fd5b9b3107bcdd23340a06a33f2e357
= <='danadev94@gmail.com>
2022-01-25 17:35:01 +0330

    Added Electrical Engineering.

--- src/config.js
+++ src/config.js
@@ -1,10 +1,11 @@
 export default {
     currentSemester:'1400-02',
-    majors:['Math','CivilEng']
+    majors:['Math','CivilEng','ElecEng']
 }
 
 export let majorsList= [
     ['CivilEng','مهندسی عمران','mdi-account-hard-hat'],
     ['Math','ریاضیات و کاربرد ها','mdi-math-integral'],
-    ['MechEng','مهندسی مکانیک','mdi-tools']
+    ['MechEng','مهندسی مکانیک','mdi-tools'],
+    ['ElecEng','مهندسی برق','mdi-flash']
 ]
