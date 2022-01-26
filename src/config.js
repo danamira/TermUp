c35000656a49a7d2dedcf4dc1c1239753ae83acc
= <='danadev94@gmail.com>
2022-01-26 23:25:46 +0330

    Added compEng.

--- src/config.js
+++ src/config.js
@@ -1,11 +1,12 @@
 export default {
     currentSemester:'1400-02',
-    majors:['Math','CivilEng','ElecEng']
+    majors:['Math','CivilEng','ElecEng','CompEng']
 }
 
 export let majorsList= [
     ['CivilEng','مهندسی عمران','mdi-account-hard-hat'],
     ['Math','ریاضیات و کاربرد ها','mdi-math-integral'],
     ['MechEng','مهندسی مکانیک','mdi-tools'],
-    ['ElecEng','مهندسی برق','mdi-flash']
+    ['ElecEng','مهندسی برق','mdi-flash'],
+    ['CompEng','مهندسی کامپیوتر','mdi-code-array']
 ]
