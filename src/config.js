71bb5afcc5d53289dacf08a67233429bffc62a48
= <='danadev94@gmail.com>
2022-01-29 01:25:42 +0330

    Adde Polimer Engineering.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
     currentSemester:'1400-02',
-    majors:['CivilEng','UrbnEng','IndEng','Econ','ElecEng','Mngmnt','MechEng','Metallurgy','CompEng','EngSci','Math','OilEng','Geography','MineEng','Psychology','SurvEng','ChemEng','Arch']
+    majors:['CivilEng','PolEng','UrbnEng','IndEng','Econ','ElecEng','Mngmnt','MechEng','Metallurgy','CompEng','EngSci','Math','OilEng','Geography','MineEng','Psychology','SurvEng','ChemEng','Arch']
 }
 
 export let majorsList= [
@@ -11,12 +11,13 @@ export let majorsList= [
     ['CompEng','مهندسی کامپیوتر','mdi-code-array'],
     ['ChemEng','مهندسی شیمی','mdi-flask'],
     ['Metallurgy','مهندسی مواد','mdi-cup'],
-    ['OilEng','مهندسی نفت','mdi-barrel'],
     ['Math','ریاضیات و کاربرد ها','mdi-math-integral'],
+    ['OilEng','مهندسی نفت','mdi-barrel'],
     ['Arch','معماری','mdi-math-compass'],
     ['EngSci','علوم مهندسی','mdi-rocket'],
     ['SurvEng','مهندسی نقشه برداری','mdi-map'],
     ['MineEng','مهندسی معدن','mdi-gantry-crane'],
+    ['PolEng','مهندسی پلیمر','mdi-seed'],
     ['UrbnEng','مهندسی شهرسازی','mdi-city-variant-outline'],
     ['Geography','جغرافیا','mdi-earth'],
     ['Psychology','روانشناسی','mdi-brain'],
