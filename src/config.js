1d6b0f2ad638cb1023045bfe1bab28dc2071c001
danamira <danadev94@gmail.com>
2022-08-27 16:19:37 +0430

    Added data: Math | 1401-01.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   sems:[
-    {code:"1401-01","title":"ترم پاییز 1401",majors:["ElecEng","MechEng","CivilEng","LangEng","ChemEng","Psychology","MineEng","OilEng","CS","Stats","IndEng","ArchEng","PolEng","EngSci","Geography","Physics","CompEng","PhilosophyFar","Metallurgy"]},
+    {code:"1401-01","title":"ترم پاییز 1401",majors:["ElecEng","MechEng","CivilEng","LangEng","ChemEng","Psychology","MineEng","OilEng","CS","Stats","IndEng","ArchEng","PolEng","EngSci","Geography","Physics","CompEng","PhilosophyFar","Metallurgy","Math"]},
     {
       code:"1400-02",
       title:"ترم زمستان 1400",
